f3bc2b6f20ecffc8d218c6ebae81cccfb5e55bd9
unknown <francobaez2309@gmail.com>
2023-08-17 23:13:50 -0300

    Commit de clase Fecha

---
+++ Fecha/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include "fecha.h"
+
+int main()
+{
+    Fecha p1(1, 3, 2023);
+
+    std::cout<<p1.to_string()<<std::endl;
+
+    p1.agregarDias(-1);
+
+    std::cout<<p1.to_string()<<std::endl;
+    
+    
+    system("PAUSE");
+    
+} 
+
+//cd "c:\Users\Franco\FACULTAD\Poo\Fecha\" ; if ($?) { g++ *.cpp -o main } ; if ($?) { .\main }
